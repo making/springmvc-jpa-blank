dea82e326a7f95a584ee18cc9654bfe3ffffb0ac
Toshiaki Maki <makingx@gmail.com>
2011-12-31 17:14:59 +0900

    modified JPQL a little

--- src/main/java/com/example/mvc/repository/custom/PersonRepositoryCustom.java
+++ src/main/java/com/example/mvc/repository/custom/PersonRepositoryCustom.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.example.mvc.entity.Person;
 
 public interface PersonRepositoryCustom {
-    @Query("SELECT p FROM Person WHERE name LIKE ?1")
+    @Query("SELECT p FROM Person p WHERE name LIKE ?1")
     Page<Person> findByNameLike(String name, Pageable pageable);
 }
